d166bad0f9cf16403433e7149a40d99cddae1fd1
jonatas <jonatas_dev@hotmail.com>
2025-12-10 21:47:15 -0300

    Feat: Inicial desafios

---
+++ src/stream__api/desafios/Desafio02.java
@@ -0,0 +1,25 @@
+package stream__api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+//Desafio 2 - Imprima a soma dos números pares da lista:
+//Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+public class Desafio02 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> divisor =  numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+
+        int somandoValores = divisor.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        //imprimindo
+        System.out.println(somandoValores);
+    }
+}
